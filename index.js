3653353a4d416f00b97ed791cff97c37a9964ada
Cyusa Munezero Keny <gasananike@gmail.com>
2022-01-16 21:37:28 +0200

    redis working perfectly

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const app = require('express')()
 const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-io.on('connection',socket =>{
-    socket.on('Calculate',()=>{
-        socket.emit('doit');  
-    });
-})
+const redis = require('redis');
+const client = redis.createClient({
+    host: 'localhost',
+    port: 4500
+});
+
+client.connect();
+
+client.on('error', err => console.log('REDIS ERROR: ', err));
+
+client.on("connect", ()=> console.log("REDIS CLIENT CONNECTED TO SERVER"));
+
+client.ZADD("scores", {score: 80, value: 'player2'});
+client.ZADD("speeds",{score:24, value:'player2'});
+client.ZADD("accuracy",{score:9,value:'player2'});
+let personInfo=[];
+const data= async()=>{
+    let scoreresult = await client.ZRANGE_WITHSCORES('scores', 0,-1)
+    let speedsresult = await client.ZRANGE_WITHSCORES('speeds', 0,-1)
+    let accuracyresult = await client.ZRANGE_WITHSCORES('accuracy', 0,-1)
+    console.log("Result:",scoreresult)
+    console.log("Speeds:",speedsresult)
+    console.log("Accuracy:",accuracyresult)
+   for (const score of scoreresult) {
+    let Myinfo ={
+        id : score.value,
+        name :await client.ZSCORE('accuracy',score.value),
+        speed : await client.ZSCORE('speeds',score.value)
+    }  
+       personInfo.push(Myinfo);
+   }
+   console.log("Our winners",personInfo);
+}
+data();
+
 http.listen(5000,function (){
     console.log("Server listening on port 5000")
 })
---
+++ redis-server.js
@@ -0,0 +1,17 @@
+const RedisServer = require('redis-server');
+
+const PORT = 4500;
+
+const server = new RedisServer({
+    port: PORT,
+    bin: '/usr/bin/redis-server'
+  });
+ 
+server.open()
+.then(()=> {
+    console.log(`Redis Server started on port ${PORT}`);
+    server.on("connection", (stream)=> {
+        console.log("A NEW REDIS CLIENT CONNECTED");
+    });
+})
+.catch(e=> console.log(`REDIS SERVER START ERROR: ${e.message}`));
