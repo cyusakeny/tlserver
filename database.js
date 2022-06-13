18c65443d0605cbc336ba98897400e4cc0db8fc3
Cyusa Munezero Keny <gasananike@gmail.com>
2022-06-13 14:31:11 +0200

    live working perfectly

--- database.js
+++ database.js
@@ -1,7 +1,14 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize('tldb', 'mbird', 'frenchlick', {
-    host: 'localhost',
+const db = new Sequelize('d1fal5qid7h6fr', 'xxbjyxhxyqzwqw', 
+'7dfc876ad0148d3d15e0ec6c446e3fa5965585a60e65b25bae925b3d7d3dbb44', {
+    host: 'ec2-34-197-84-74.compute-1.amazonaws.com',
     dialect:'postgres',
-    port:5432
+    port:5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+      }
   });
 module.exports = db;
